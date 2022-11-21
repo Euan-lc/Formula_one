0b30e76160b78a6e036d2d36d130d72bd1c92923
Euan-lc <e.cockburn@students.ephec.be>
2022-11-21 13:53:01 +0100

    Update Makefile and main.c

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-main: main.c libs/car.h libs/display.c libs/vroum.c libs/sort.c
+all: main.c libs/car.h libs/display.c libs/vroum.c libs/sort.c
 	gcc libs/display.c -c
 	gcc libs/sort.c -c
 	gcc libs/vroum.c -c
--- main.c
+++ main.c
@@ -1,23 +1,27 @@
 #include "libs/f1.h"
 
+//TODO: fix display only updating time once
+//TODO: different behaviour based on type of race (Q1-3, P1-3, s)
+//TODO: saving results to csv
+//TODO: starting cars based on results
+
 void main(int argc, char *argv[]) {
     int shmid, cpid, num_cars, shmkey = 420;
+    num_cars = atoi(argv[1]);
 
     //generate seed for rand
     srand48(time(0));
-    num_cars = atoi(argv[1]);
 
     //display
     initscr();
     start_color();
     init_pair(1,COLOR_MAGENTA, COLOR_BLACK);
 
-
     //shared memory
     shmid = shmget(shmkey, num_cars * sizeof(car), IPC_CREAT | 0666);
     car *circuit = shmat(shmid, 0, 0);
 
-    printf("%d\n",num_cars);
+    //printf("%d\n",num_cars);
 
     for (int i = 0; i < num_cars; i++) {
         init_car(&circuit[i], carIds[i]);
@@ -28,27 +32,28 @@ void main(int argc, char *argv[]) {
             car child;
             child = circuit[i];
             //printf("[son] pid %d from [parent] pid %d and car id is %d\n", getpid(), getppid(), child.id);
-            for(int i = 0; i < 50; i++){
-                sleep(0.5);
+            for(int i = 0; i < 20; i++){
+                sleep(0.25);
                 lap_car(&circuit[i]);
             }
             //printf("car %d has a lap time of %g\n", child.id, child.total_time);
             exit(0);
-        }else{
-            halfdelay(5);
-            for(int i = 0; i < 10; i++) {
-                car * buffer = malloc(num_cars * sizeof(car));
-                memcpy(buffer,circuit,num_cars * sizeof(car));
-                //bubble_sort(buffer, num_cars);
-
-                display_scores(buffer, num_cars);
-
-                getch();
-                erase();
-                refresh();
-            }
         }
     }
+
+    halfdelay(5);
+    for(int i = 0; i < 10; i++) {
+        //car * buffer = malloc(num_cars * sizeof(car));
+        //memcpy(buffer,circuit,num_cars * sizeof(car));
+        //bubble_sort(buffer, num_cars);
+
+        display_scores(circuit, num_cars);
+
+        getch();
+        erase();
+        refresh();
+    }
+
     //shared memory
     shmctl(shmid, IPC_RMID, NULL);
     shmdt(circuit);
