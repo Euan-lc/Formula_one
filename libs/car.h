04940a2a6a1f0f1d92ca559905812b53586ae045
Martin-Hayot <116266177+Martin-Hayot@users.noreply.github.com>
2023-01-02 16:46:14 +0100

    Update car.h
    add state_crash

--- libs/car.h
+++ libs/car.h
@@ -5,7 +5,8 @@ typedef struct{
     int has_best_s1, has_best_s2, has_best_s3;
     float best_lap;
     float total_time;
-    bool state_pitstop_crash;
+    bool state_pitstop;
+    bool state_crash;
 }car;
 
 static int carIds[] = {44, 63, 1, 11, 55, 16, 4, 3, 14, 31, 10, 22,
